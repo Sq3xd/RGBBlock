821b099631776a5c4afc192744a8ad16a2c08cf0
sq3xd <JustDeverXD@gmail.com>
2022-09-13 20:57:02 +0200

    Release

---
+++ src/main/java/com/sq3xd/brgb/BRGBMod.java
@@ -0,0 +1,36 @@
+package com.sq3xd.brgb;
+
+import com.mojang.logging.LogUtils;
+import com.sq3xd.brgb.block.ModBlocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+ // The value here should match an entry in the META-INF/mods.toml file
+@Mod(BRGBMod.MOD_ID)
+public class BRGBMod
+{
+     // Directly reference a slf4j logger
+    public static final String MOD_ID = "brgb";
+
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public BRGBMod()
+    {
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModBlocks.register(eventBus);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+        LOGGER.info("Successfully loaded world with RGB Block mod!");
+    }
+}
---
+++ src/main/java/com/sq3xd/brgb/block/ModBlocks.java
@@ -0,0 +1,36 @@
+package com.sq3xd.brgb.block;
+
+import com.sq3xd.brgb.BRGBMod;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.ToIntFunction;
+
+
+public class ModBlocks {
+    public static ToIntFunction<BlockState> lightLevel = BlockState -> 5;
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, BRGBMod.MOD_ID);
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, BRGBMod.MOD_ID);
+
+    public static final RegistryObject<Block> RGB_BLOCK = BLOCKS.register("rgb_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).lightLevel(lightLevel).explosionResistance(6f).destroyTime(1.25f)
+                    .speedFactor(1.25f).requiresCorrectToolForDrops()));
+
+    public static final RegistryObject<Item> RGB_BLOCK_ITEM = ITEMS.register("rgb_block",
+            () -> new BlockItem(RGB_BLOCK.get(), new Item.Properties().tab(CreativeModeTab.TAB_DECORATIONS)));
+
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+        BLOCKS.register(eventBus);
+    }
+}
